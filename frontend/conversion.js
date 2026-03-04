1beedb04fb6621164292a28dff18a53607f69551
Priyanshu Chaubey <121727243+solvewitcode@users.noreply.github.com>
2026-03-04 21:29:59 +0000

    part 2

---
+++ frontend/conversion.js
@@ -0,0 +1,24 @@
+// let score = '33abc'
+// console.log(typeof score)
+
+// let numscore = Number(score)
+// console.log(typeof numscore)
+// console.log(numscore) // gives nan
+
+// -----------------------------------------------------------
+
+// let score = null
+// console.log(typeof score)
+
+// let numscore = Number(score)
+// console.log(typeof numscore)
+// console.log(numscore) // gives 0
+
+// -----------------------------------------------------------
+
+// let score = undefined  
+// console.log(typeof score)
+
+// let numscore = Number(score)
+// console.log(typeof numscore)
+// console.log(numscore) // gives nan
